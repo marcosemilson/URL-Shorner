c228954548dd1c91b5455a29f2d0dbf92ffe996b
marcosenpereira <marcosenpereira@gmail.com>
2022-02-22 00:31:54 -0300

    URL shortner project

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://<user>:<password>@shortner-url.z9u30.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
